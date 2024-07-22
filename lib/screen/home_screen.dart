67bdfbcc62a77137b0bf2b9de24af6b49a6efa09
Yujin <tkfkd7038@naver.com>
2024-07-22 22:16:13 +0900

    Feat: 위치권한 관리 함수 생성

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -68,4 +68,28 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: Colors.white,
     );
   }
+
+  Future<String> checkPermission() async {
+    final isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if (!isLocationEnabled) {
+      return '위치 서비스를 활성화해주세요';
+    }
+
+    LocationPermission checkedPermission = await Geolocator.checkPermission();
+
+    if (checkedPermission == LocationPermission.denied) {
+      checkedPermission = await Geolocator.requestPermission();
+
+      if (checkedPermission == LocationPermission.denied) {
+        return '위치권한을 허가해주세요';
+      }
+    }
+
+    if (checkedPermission == LocationPermission.deniedForever) {
+      return '앱의 위치 권한을 설정에서 허가해주세요.';
+    }
+
+    return '위치 권한이 허가 되었습니다.';
+  }
 }
