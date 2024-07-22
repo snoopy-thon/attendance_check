14c6da94b20fde2fe8b5e15c8215d29404d12f0f
Yujin <tkfkd7038@naver.com>
2024-07-22 21:30:55 +0900

    Feat: 화면에 구글맵 추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
+import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  GoogleMapsFlutterPlatform mapsImplementation =
+      GoogleMapsFlutterPlatform.instance = GoogleMapsPlugin();
+  static const LatLng companyLatLng = LatLng(
+    37.5233273,
+    126.921252,
+  );
+
+  HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: renderAppBar(),
+      body: const GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: companyLatLng,
+          zoom: 16,
+        ),
+      ),
     );
   }
 
